1f749ede02d70d228c33002c0fa9edcf92d6e1d4
tarunpandey45 <ptarunpandey337@gmail.com>
2026-03-23 14:22:01 +0530

    Initialize index.jsx with React and Bootstrap setup
    Set up React application with Bootstrap and React Router.

--- index.jsx
+++ index.jsx
@@ -1,9 +1,15 @@
-<html>
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css';        //  system k node module k ander downlorded css ko conect karne wala
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';    //  system k node module k ander downlorded js ko conect karne wala
+import { BrowserRouter } from 'react-router-dom';    //  system k node module k ander downlorded js ko conect karne wala
 
 
+createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+)
 
-
-
-
-
-</html>
